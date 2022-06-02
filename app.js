5790cdd95aef31cb8302f6b4b1486622c1c2196f
Amy Woodworth <woodworthcommunications@gmail.com>
2022-06-02 17:10:14 -0500

    Add form error message. Finish js.

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const ratingForm = document.forms['support-feedback'];
 const ratings = ratingForm.elements.rating;
-const userRating = ratings.value;
-const noRating = "0";
+let userRating = '';
 const userRatingText = document.querySelector('.user-rating');
 const submitBtn = document.querySelector('.form-group-btn button');
+const errorMessage = document.querySelector('.error');
 const surveySection = document.getElementById('survey');
 const thankyouSection = document.getElementById('thankyou');
 
-ratings.forEach(rating => {
-  rating.addEventListener('change', () => {
-    userRatingText.innerText = rating.value;
-    console.log(userRatingText.innerText);
-  });
+getRating();
+
+submitBtn.addEventListener('click', (e) => {
+  if (userRating === '') {
+    e.preventDefault();
+    errorMessage.classList.remove('hidden');
+    errorMessage.setAttribute('aria-live', 'polite');
+    getRating();
+  } else {
+    e.preventDefault();
+    userRatingText.innerText = userRating;
+    thankyouSection.classList.remove('hidden');
+    surveySection.classList.add('hidden');
+  }
 });
+
+function getRating() {
+  ratings.forEach(rating => {
+    rating.addEventListener('change', () => {
+      userRating = rating.value;
+      return userRating;
+    });
+  });
+}
